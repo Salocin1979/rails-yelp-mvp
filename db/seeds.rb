75c68b57f9e92e4e1cdd842561de17da41f4a9ef
Nicolas Bagnath <nicolas.bagnath@gmail.com>
2021-09-28 21:23:50 +0400

    Yelp mvp final

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Creating seeds"
+
+restaurant_one = Restaurant.create(name: "Apo Riviere Noire", address: "Riviere Noire", phone_number: "483 7599", category: "chinese")
+restaurant_two = Restaurant.create(name: "Al dente", address: "Black River", phone_number: "483 7919", category: "italian")
+restaurant_three = Restaurant.create(name: "Pavillon de jade", address: "La preneuse", phone_number: "483 5860", category: "japanese")
+restaurant_four = Restaurant.create(name: "Le Kiosk", address: "Ruisseau Creole", phone_number: "483 7004", category: "french")
+restaurant_five = Restaurant.create(name: "Pasta Marin", address: "La Balise Marina", phone_number: "483 2009", category: "belgian")
+
+puts "Seeds created!"
